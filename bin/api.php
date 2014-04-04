5f2d935d968c9a63309bb639791e808d905b1a73
Robert Kummer <rok@ipunkt.biz>
2014-04-04 14:31:47 +0200

    composer.lock added alias for commands integrated

--- bin/api.php
+++ bin/api.php
@@ -8,6 +8,8 @@
  */
 require_once '../vendor/autoload.php';
 
+use \Rokde\Gsales\Api\Console\Command as ConsoleCommands;
+
 $config = include('../config.php');
 $wsdl = $config['gsales']['wsdl'];
 $apikey = $config['gsales']['apikey'];
@@ -15,7 +17,7 @@ $apikey = $config['gsales']['apikey'];
 $client = new \Rokde\Gsales\Api\Client($wsdl, $apikey);
 
 $application = new \Symfony\Component\Console\Application('gsales.api.console', '0.0.1');
-$application->add(new \Rokde\Gsales\Api\Console\Command\Customer\GetCommand($client));
-$application->add(new \Rokde\Gsales\Api\Console\Command\Customer\ListCommand($client));
-$application->add(new \Rokde\Gsales\Api\Console\Command\Customer\CountCommand($client));
+$application->add(new ConsoleCommands\Customer\GetCommand($client));
+$application->add(new ConsoleCommands\Customer\ListCommand($client));
+$application->add(new ConsoleCommands\Customer\CountCommand($client));
 $application->run();
