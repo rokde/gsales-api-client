47434f69c1821717ff5b0d988f0aabc0d303885b
Robert Kummer <rok@ipunkt.biz>
2014-11-13 15:40:04 +0100

    added config.php file for the use in laravel

---
+++ src/config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * config settings for your setup
+ */
+return array(
+	/**
+	 * URL to your gsales installation
+	 * e.g.: http://your-server.com/api/api?wsdl
+	 */
+	'wsdl' => '',
+
+	/**
+	 * Your api key for the gsales API. You have to enable it first within your installation of gsales.
+	 */
+	'apikey' => '',
+
+);
